82b669791ae01325d05bb2d5582b54cbbd08d74b
HigasiyamaHuyumi <141430431+HigashiyamaHuyumi@users.noreply.github.com>
2024-04-26 10:49:51 +0000

    db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+  email: 'admin@mail', # 任意のメールアドレス
+  password: '012345' # 任意のパスワード
+)
